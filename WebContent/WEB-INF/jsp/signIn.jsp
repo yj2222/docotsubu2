7a6fd617e6f12240f37a374a24c887726f01a192
Yuji Masujima <masjima2712@gmail.com>
2019-11-20 16:25:39 +0900

    Update SignIn Page. Create Vallidation made with Javascript.

--- WebContent/WEB-INF/jsp/signIn.jsp
+++ WebContent/WEB-INF/jsp/signIn.jsp
@@ -18,14 +18,17 @@
 						<h3 class="title">Sign In</h3>
 						<form action="/docoTsubu2/SignInServlet" method="post" id="form">
 							<p>・ニックネーム:<span style="font-size:12px;">(半角英数字4~8字,記号不可)</span></p>
-							<input type="text" name="nickName" placeholder=" 例）user1234" id="formText"><br>
-							<p class="error_msg"></p>
+							<input type="text" name="nickName" placeholder=" 例）user1234" id="formNickName">
+							<p id="errorMsg"></p>
+							<%-- <p class="errorMsg" id="formNickName_errorMsg"></p> --%>
 							<p>・メールアドレス:</p>
-							<input type="email" name="email" placeholder=" 例）test1@ex.com" id="formEmail"><br>
-							<p class="error_msg"></p>
+							<input type="email" name="email" placeholder=" 例）test1@ex.com" id="formEmail">
+							<p id="errorMsg"></p>
+							<%-- <p class="errorMsg" id="formEmail_errorMsg"></p> --%>
 							<p>・パスワード:<span	 style="font-size:12px;">(<strong style="color:pink;">記号</strong>,半角英数字を含む4~8字)</span></p>
-							<input type="password" name="password"  placeholder=" 例）pass+!-1" id="formPass"><br>
-							<p class="error_msg"></p>
+							<input type="password" name="password"  placeholder=" 例）pass+!-1" id="formPass">
+							<p id="errorMsg"></p>
+							<%-- <p class="errorMsg" id="formPass_errorMsg"></p> --%>
 							<button type="submit">Sign In</button>
 						</form>
 					</div>
--- WebContent/js/signIn.js
+++ WebContent/js/signIn.js
@@ -1,33 +1,49 @@
 window.onload = function(){
 	var form = document.getElementById('form');
-	var formText = document.getElementById('formText');
+	var formNickName = document.getElementById('formNickName');
 	var formEmail = document.getElementById('formEmail');
 	var formPass = document.getElementById('formPass');
 	var validationMessage;
 
-	formText.addEventListener('change', (e) => {
-		var formTextVal = formText.value;
-		var msg = checkHalfWidthChara(formTextVal);
-		console.log(msg);
+	formNickName.addEventListener('change', (e) => {
+		var formNickNameVal = formNickName.value;
+		var msgFlag = checkHalfWidthChara(formNickNameVal);
+		validationMessage = putErorrMsg(msgFlag);
+		ChangeText(formNickName, validationMessage);
 	});
 
 	formEmail.addEventListener('change', (e) => {
-//		var type = e.target.type;
-		validationMessage = e.target.validationMessage;
-		console.log(validationMessage);
+		var type = e.target.type;
+		validationMessage = "※" + e.target.validationMessage;
+		ChangeText(formEmail, validationMessage);
 	});
 
 	formPass.addEventListener('change', (e) => {
 		var formPassVal = formPass.value;
 		var msgFlag = checkIncludeSymbol(formPassVal);
-		console.log(msgFlag);
 		validationMessage = putErorrMsg(msgFlag);
-		console.log(validationMessage);
+		ChangeText(formPass, validationMessage);
 	});
 }
 
 function checkHalfWidthChara(val){
-
+	var flag = 0;
+	if(val.length >= 4 && val.length <= 8){
+		if(val.match(/[^a-zA-Z0-9]/)){
+			flag = 2;
+		}else if( val.match(/[a-zA-Z0-9]/) ){
+			flag = 1;
+		} else{
+			flag = 3;
+		}
+	} else {
+		if(val.length == 0){
+			flag = 4;
+		}else if(val.length <= 3 || val.length >= 9){
+			flag = 5;
+		}
+	}
+	return flag;
 }
 
 function checkIncludeSymbol(val){
@@ -57,18 +73,23 @@ function putErorrMsg(num){
 	    	erorrMsg = "";
 	        break;
 	    case 2:
-	    	erorrMsg = "使えない文字が含まれています。";
+	    	erorrMsg = "※使えない文字が含まれています。";
 	        break;
 	    case 3:
-	    	erorrMsg = "条件を満たしていません。";
+	    	erorrMsg = "※条件を満たしていません。";
 	        break;
 	    case 4:
-	    	erorrMsg = "入力してください。";
+	    	erorrMsg = "※入力してください。";
 	        break;
 	    case 5:
-	    	erorrMsg = "入力が長すぎるか、短すぎます。";
+	    	erorrMsg = "※入力が長すぎるか、短すぎます。";
 	        break;
 	}
 	return erorrMsg;
 }
+
+function ChangeText(ele, txt) {
+	ele.nextElementSibling.innerHTML = txt;
+}
+
 
