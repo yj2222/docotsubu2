fc5940d8e367b00f8d46a41c060fbb54e0ff13ad
Yuji Masujima <masjima2712@gmail.com>
2019-11-20 16:55:34 +0900

    Update SignIn file. Add Coment.

--- WebContent/WEB-INF/jsp/signIn.jsp
+++ WebContent/WEB-INF/jsp/signIn.jsp
@@ -3,12 +3,14 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
+		<%-- JSPからは、${pageContext.request.contextPath}を指定することでWebContent配下を読込める。--%>
 		<link rel="stylesheet" href="${pageContext.request.contextPath}/css/reset.css">
 		<link rel="stylesheet" href="${pageContext.request.contextPath}/css/signIn.css">
 		<script type="text/javascript" src="${pageContext.request.contextPath}/js/signIn.js"></script>
 		<title>どこつぶ改！！！</title>
 	</head>
 	<body>
+
 		<div class="background-image"
 		 style="background-image: url(${pageContext.request.contextPath}/image/signIn.jpg);
 		  background-size: cover">
@@ -20,15 +22,12 @@
 							<p>・ニックネーム:<span style="font-size:12px;">(半角英数字4~8字,記号不可)</span></p>
 							<input type="text" name="nickName" placeholder=" 例）user1234" id="formNickName">
 							<p id="errorMsg"></p>
-							<%-- <p class="errorMsg" id="formNickName_errorMsg"></p> --%>
 							<p>・メールアドレス:</p>
 							<input type="email" name="email" placeholder=" 例）test1@ex.com" id="formEmail">
 							<p id="errorMsg"></p>
-							<%-- <p class="errorMsg" id="formEmail_errorMsg"></p> --%>
 							<p>・パスワード:<span	 style="font-size:12px;">(<strong style="color:pink;">記号</strong>,半角英数字を含む4~8字)</span></p>
 							<input type="password" name="password"  placeholder=" 例）pass+!-1" id="formPass">
 							<p id="errorMsg"></p>
-							<%-- <p class="errorMsg" id="formPass_errorMsg"></p> --%>
 							<button type="submit">Sign In</button>
 						</form>
 					</div>
--- WebContent/js/signIn.js
+++ WebContent/js/signIn.js
@@ -1,31 +1,39 @@
 window.onload = function(){
+	// 各フォームの要素を指定。
 	var form = document.getElementById('form');
 	var formNickName = document.getElementById('formNickName');
 	var formEmail = document.getElementById('formEmail');
 	var formPass = document.getElementById('formPass');
 	var validationMessage;
 
+	// ニックネームフォームのバリデーション。
 	formNickName.addEventListener('change', (e) => {
 		var formNickNameVal = formNickName.value;
+		// 半角英数字のチェック。チェック後フラグに応じて出力メッセージを選定。
 		var msgFlag = checkHalfWidthChara(formNickNameVal);
 		validationMessage = putErorrMsg(msgFlag);
 		ChangeText(formNickName, validationMessage);
 	});
 
+	// メールフォームのバリデーション。
 	formEmail.addEventListener('change', (e) => {
 		var type = e.target.type;
+		// デフォルトのバリデーションメッセージを使用。
 		validationMessage = "※" + e.target.validationMessage;
 		ChangeText(formEmail, validationMessage);
 	});
 
+	// パスワードフォームのバリデーション。
 	formPass.addEventListener('change', (e) => {
 		var formPassVal = formPass.value;
+		// 半角英数字＋記号が含まれているかチェック。チェック後フラグに応じて出力メッセージを選定。
 		var msgFlag = checkIncludeSymbol(formPassVal);
 		validationMessage = putErorrMsg(msgFlag);
 		ChangeText(formPass, validationMessage);
 	});
 }
 
+// 半角のチェック関数。
 function checkHalfWidthChara(val){
 	var flag = 0;
 	if(val.length >= 4 && val.length <= 8){
@@ -46,6 +54,7 @@ function checkHalfWidthChara(val){
 	return flag;
 }
 
+//半角＋記号のチェック関数。
 function checkIncludeSymbol(val){
 	var flag = 0;
 	if(val.length >= 4 && val.length <= 8){
@@ -66,6 +75,7 @@ function checkIncludeSymbol(val){
 	return flag;
 }
 
+// バリデーションメッセージ選定の関数。
 function putErorrMsg(num){
 	var erorrMsg;
 	switch(num) {
@@ -88,6 +98,7 @@ function putErorrMsg(num){
 	return erorrMsg;
 }
 
+// バリデーションメッセージのHTMLのテキスト書き換え関数。
 function ChangeText(ele, txt) {
 	ele.nextElementSibling.innerHTML = txt;
 }
