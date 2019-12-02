4591845a402b87498a8e8dc083ca755e42e85ffe
Yuji Masujima <masjima2712@gmail.com>
2019-12-02 14:05:16 +0900

    update pom for deploy

--- src/dao/UserDAO_Production.java
+++ src/dao/UserDAO_Production.java
@@ -1,5 +1,7 @@
 package dao;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -15,68 +17,61 @@ public class UserDAO_Production {
 
     String username = dbUri.getUserInfo().split(":")[0];
     String password = dbUri.getUserInfo().split(":")[1];
-		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-		
+		String dbUrl = "jdbc:mariadb://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
     return DriverManager.getConnection(dbUrl, username, password);
 	}
 
 	public boolean create(User user) {
-		try {
-			Class.forName ("org.h2.Driver");
-
-		    try(Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
-		    	String sql = "INSERT INTO USER (NICKNAME, EMAIL, PASSWORD) VALUES(?, ?, ?)";
-		        PreparedStatement pStmt = conn.prepareStatement(sql);
-		        pStmt.setString(1, user.getNickName());
-		        pStmt.setString(2, user.getEmail());
-		        pStmt.setString(3, user.getPassword());
-		        int result = pStmt.executeUpdate();
-		        if (result != 1) {
-		          return false;
-		        }
-		    } catch (SQLException e) {
-		        e.printStackTrace();
-		        return false;
-		    }
-		} catch (ClassNotFoundException e1) {
-			e1.printStackTrace();
-			return false;
-		}
+	  try(Connection conn = getConnection()) {
+	  	String sql = "INSERT INTO USER (NICKNAME, EMAIL, PASSWORD) VALUES(?, ?, ?)";
+	      PreparedStatement pStmt = conn.prepareStatement(sql);
+	      pStmt.setString(1, user.getNickName());
+	      pStmt.setString(2, user.getEmail());
+	      pStmt.setString(3, user.getPassword());
+	      int result = pStmt.executeUpdate();
+	      if (result != 1) {
+	        return false;
+	      }
+	  } catch (URISyntaxException e) {
+	      e.printStackTrace();
+	      return false;
+	  } catch (SQLException e) {
+	    e.printStackTrace();
+	    return false;
+	  }
 		return true;
 	}
 
 	public User findUser(User user) {
 		User findedUser = null;
-		try {
-			Class.forName ("org.h2.Driver");
-				// データベースへ接続
-		    try(Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
-		    	// SELECT文を準備
-		    	String sql = "SELECT * FROM user WHERE nickname = ? AND password = ?";
-	        PreparedStatement pStmt = conn.prepareStatement(sql);
-	        pStmt.setString(1, user.getNickName());
-	        pStmt.setString(2, user.getPassword());
-	        // SELECTを実行し、結果表を取得
-	        ResultSet rs = pStmt.executeQuery();
-	        // 一致したユーザーが存在した場合
-	        // そのユーザーを表すAccountインスタンスを生成
-	        if (rs.next()) {
-	          // 結果表からデータを取得
-	          String nickName = rs.getString("nickname");
-	          String email = rs.getString("email");
-	          String password = rs.getString("password");
-	          findedUser = new User(nickName, email, password);
-	        }
-		    } catch (SQLException e) {
-		        e.printStackTrace();
-		        return null;
-		    }
-		} catch (ClassNotFoundException e1) {
-			e1.printStackTrace();
-			return null;
-		}
+			// データベースへ接続
+	    try(Connection conn = getConnection()) {
+	    	// SELECT文を準備
+	    	String sql = "SELECT * FROM user WHERE nickname = ? AND password = ?";
+	      PreparedStatement pStmt = conn.prepareStatement(sql);
+	      pStmt.setString(1, user.getNickName());
+	      pStmt.setString(2, user.getPassword());
+	      // SELECTを実行し、結果表を取得
+	      ResultSet rs = pStmt.executeQuery();
+	      // 一致したユーザーが存在した場合
+	      // そのユーザーを表すAccountインスタンスを生成
+	      if (rs.next()) {
+	        // 結果表からデータを取得
+	        String nickName = rs.getString("nickname");
+	        String email = rs.getString("email");
+	        String password = rs.getString("password");
+	        findedUser = new User(nickName, email, password);
+	      }
+	    } catch (URISyntaxException e) {
+	        e.printStackTrace();
+	        return null;
+	    } catch (SQLException e) {
+	      e.printStackTrace();
+	      return null;
+	    }
 		return findedUser;
 	}
-	
+
 }
 
