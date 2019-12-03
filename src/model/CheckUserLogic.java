4710cc4f295edb920c4be7c6e3aa17bc4f72342e
Yuji Masujima <masjima2712@gmail.com>
2019-12-03 14:20:57 +0900

    production環境様のDAOを呼び出し。

--- src/model/CheckUserLogic.java
+++ src/model/CheckUserLogic.java
@@ -1,10 +1,14 @@
 package model;
 
-import dao.UserDAO;
+import dao.UserDAO_Production;
 
 public class CheckUserLogic {
-	public User execute(User user) {
-    UserDAO dao = new UserDAO();
-    return dao.findUser(user);
+//	public User execute(User user) {
+//    UserDAO dao = new UserDAO();
+//    return dao.findUser(user);
+//	}
+	public User executeProduction(User user) {
+    UserDAO_Production daoProduction = new UserDAO_Production();
+    return daoProduction.findUser(user);
 	}
 }
