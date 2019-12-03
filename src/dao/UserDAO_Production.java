ed99ad99845e1ce18609a2a29c391c2b6e7b3e43
Yuji Masujima <masjima2712@gmail.com>
2019-12-03 14:52:44 +0900

    MySQLドライバーの導入

--- src/dao/UserDAO_Production.java
+++ src/dao/UserDAO_Production.java
@@ -22,52 +22,59 @@ public class UserDAO_Production {
 		return DriverManager.getConnection(dbUrl, username, password);
 	}
 
-	public boolean create(User user) {
-		try(Connection conn = getConnection()) {
-			String sql = "INSERT INTO USER (NICKNAME, EMAIL, PASSWORD) VALUES(?, ?, ?)";
-			PreparedStatement pStmt = conn.prepareStatement(sql);
-			pStmt.setString(1, user.getNickName());
-			pStmt.setString(2, user.getEmail());
-			pStmt.setString(3, user.getPassword());
-			int result = pStmt.executeUpdate();
-			if (result != 1) {
-				return false;
-			}
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-			return false;
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
+//	public boolean create(User user) {
+//		try(Connection conn = getConnection()) {
+//			String sql = "INSERT INTO USER (NICKNAME, EMAIL, PASSWORD) VALUES(?, ?, ?)";
+//			PreparedStatement pStmt = conn.prepareStatement(sql);
+//			pStmt.setString(1, user.getNickName());
+//			pStmt.setString(2, user.getEmail());
+//			pStmt.setString(3, user.getPassword());
+//			int result = pStmt.executeUpdate();
+//			if (result != 1) {
+//				return false;
+//			}
+//		} catch (URISyntaxException e) {
+//			e.printStackTrace();
+//			return false;
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//			return false;
+//		}
+//		return true;
+//	}
 
 	public User findUser(User user) {
 		User findedUser = null;
+
+		try {
+			Class.forName ("com.mysql.cj.jdbc.Driver");
 		// データベースへ接続
-		try(Connection conn = getConnection()) {
-			// SELECT文を準備
-			String sql = "SELECT * FROM account WHERE nickname = ? AND password = ?";
-			PreparedStatement pStmt = conn.prepareStatement(sql);
-			pStmt.setString(1, user.getNickName());
-			pStmt.setString(2, user.getPassword());
-			// SELECTを実行し、結果表を取得
-			ResultSet rs = pStmt.executeQuery();
-			// 一致したユーザーが存在した場合
-			// そのユーザーを表すAccountインスタンスを生成
-			if (rs.next()) {
-			// 結果表からデータを取得
-				String nickName = rs.getString("nickname");
-				String email = rs.getString("email");
-				String password = rs.getString("password");
-				findedUser = new User(nickName, email, password);
+			try(Connection conn = getConnection()) {
+				// SELECT文を準備
+				String sql = "SELECT * FROM account WHERE nickname = ? AND password = ?";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+				pStmt.setString(1, user.getNickName());
+				pStmt.setString(2, user.getPassword());
+				// SELECTを実行し、結果表を取得
+				ResultSet rs = pStmt.executeQuery();
+				// 一致したユーザーが存在した場合
+				// そのユーザーを表すAccountインスタンスを生成
+				if (rs.next()) {
+				// 結果表からデータを取得
+					String nickName = rs.getString("nickname");
+					String email = rs.getString("email");
+					String password = rs.getString("password");
+					findedUser = new User(nickName, email, password);
+				}
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+				return null;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return null;
 			}
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-			return null;
-		} catch (SQLException e) {
-			e.printStackTrace();
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
 			return null;
 		}
 		return findedUser;
