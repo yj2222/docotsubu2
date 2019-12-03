9271ebd9d76fd7f6add950308ccd3cfda80e13bc
Yuji Masujima <masjima2712@gmail.com>
2019-12-03 16:27:48 +0900

    新規ユーザ作成用の本番DAOの変更

--- src/dao/UserDAO_Production.java
+++ src/dao/UserDAO_Production.java
@@ -22,26 +22,34 @@ public class UserDAO_Production {
 		return DriverManager.getConnection(dbUrl, username, password);
 	}
 
-//	public boolean create(User user) {
-//		try(Connection conn = getConnection()) {
-//			String sql = "INSERT INTO USER (NICKNAME, EMAIL, PASSWORD) VALUES(?, ?, ?)";
-//			PreparedStatement pStmt = conn.prepareStatement(sql);
-//			pStmt.setString(1, user.getNickName());
-//			pStmt.setString(2, user.getEmail());
-//			pStmt.setString(3, user.getPassword());
-//			int result = pStmt.executeUpdate();
-//			if (result != 1) {
-//				return false;
-//			}
-//		} catch (URISyntaxException e) {
-//			e.printStackTrace();
-//			return false;
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//			return false;
-//		}
-//		return true;
-//	}
+	public boolean create(User user) {
+
+		try {
+			Class.forName ("com.mysql.cj.jdbc.Driver");
+			// データベースへ接続
+			try(Connection conn = getConnection()) {
+				String sql = "INSERT INTO USER (NICKNAME, EMAIL, PASSWORD) VALUES(?, ?, ?)";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+				pStmt.setString(1, user.getNickName());
+				pStmt.setString(2, user.getEmail());
+				pStmt.setString(3, user.getPassword());
+				int result = pStmt.executeUpdate();
+				if (result != 1) {
+					return false;
+				}
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+				return false;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return false;
+			}
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+			return false;
+		}
+		return true;
+	}
 
 	public User findUser(User user) {
 		User findedUser = null;
--- src/model/PostUserLogic.java
+++ src/model/PostUserLogic.java
@@ -1,10 +1,14 @@
 package model;
 
-import dao.UserDAO;
+import dao.UserDAO_Production;
 
 public class PostUserLogic {
+//	public boolean execute(User user) {
+//	    UserDAO dao = new UserDAO();
+//	    return dao.create(user);
+//	}
 	public boolean execute(User user) {
-	    UserDAO dao = new UserDAO();
-	    return dao.create(user);
+		UserDAO_Production daoProduction = new UserDAO_Production();
+    return daoProduction.create(user);
 	}
 }
